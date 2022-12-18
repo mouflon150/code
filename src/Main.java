a36bbba122f85d7b5b8baec39f6cbf39391ab519
Marlen Temirbaev <mouflon150@gmail.com>
2022-12-18 15:33:12 +0600

    Commit message

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Objected Oriented Programming and GitHub practice
-
-        Dog dog = new Dog("Druzhok");
-        System.out.print(dog); dog.voice();
-
-        Cat cat = new Cat("Murka");
-        System.out.print(cat); cat.voice();
 
     }
 }
--- src/Voiceable.java
+++
@@ -1,3 +0,0 @@
-public interface Voiceable {
-    void voice();
-}
