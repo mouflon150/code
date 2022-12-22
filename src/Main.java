1313d647560295e36fe229e212fdcab147d44697
Marlen Temirbaev <mouflon150@gmail.com>
2022-12-22 17:30:23 +0600

    Initial  commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
 
+        List<Student> students = new ArrayList<>();
+        try {
+            students.add(new Student("Marlen", 17));
+            students.add(new Student("Bakyt", -1));
+            System.out.println(students);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        } catch (MyException myException) {
+            System.out.println(myException.getMessage());
+        } finally {
+            System.out.println("End");
+        }
     }
 }
+
+
